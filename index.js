d1cf76ef53ba2b7fcf831f2b8c6008907b362fe8
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-02-12 13:18:46 -0300

    fix fibWn

--- index.js
+++ index.js
@@ -13,25 +13,35 @@ function fibMap(n) { ///function using Map
     return result;
 }
 const cacheWn = new WeakMap();
+const key = {};
 
 function fibWn(n) {
-  if (cacheWn.has({n: n})) {
-    console.log(`Returning cached value for n=${n}`);
-    return cacheWn.get({n: n});
+  if (cacheWn.has(key)) {
+    cacheWn.get(key)[n] = cacheWn.get(key)[n] || null;
+    if (cacheWn.get(key)[n]) {
+      //console.log(`Returning cached value for n=${n}`);
+      return cacheWn.get(key)[n];
+    }
+  } else {
+    cacheWn.set(key, {});
   }
 
   if (n <= 1) {
-    cacheWn.set({n: n}, n);
+    //console.log(`Caching value n=${n}`);
+    cacheWn.get(key)[n] = n;
     return n;
   }
 
   const result = fibWn(n - 1) + fibWn(n - 2);
-  cacheWn.set({n: n}, result);
+  cacheWn.get(key)[n] = result;
+  //console.log(`Returning calculated value for n=${n}`);
   return result;
 }
 
-console.log(fibWn(20));
 
-console.log(fibMap(100));
+
+console.log(fibWn(100));
+
+//console.log(fibMap(100));
 
 
